a66d671dc0d1bc4486a16afd8d4a68bc219705e1
luud.janssen <luud-@live.nl>
2018-02-01 16:09:08 +0100

    Add example builder

--- webpack.development.js
+++ webpack.development.js
@@ -7,7 +7,7 @@ const common = require('./webpack.common.js');
 module.exports = merge(common, {
   entry: './examples/js/examples.js',
   output: {
-    path: path.resolve(__dirname, 'tmp'),
+    path: path.resolve(__dirname, 'dist/examples'),
   },
   module: {
     rules: [
@@ -21,7 +21,7 @@ module.exports = merge(common, {
     ],
   },
   plugins: [
-    new CleanWebpackPlugin(['tmp']),
+    new CleanWebpackPlugin(['dist/examples']),
     new HtmlWebpackPlugin({
       template: './examples/index.html',
       minify: {},
@@ -29,6 +29,6 @@ module.exports = merge(common, {
   ],
   devtool: 'source-map',
   devServer: {
-    contentBase: './tmp',
+    contentBase: './dist/examples',
   },
 });
