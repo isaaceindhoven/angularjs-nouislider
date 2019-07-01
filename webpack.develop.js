872fd205de29eda4f20d3ad1cb3617cefda3c62d
Luud Janssen <luud-@live.nl>
2019-07-01 11:04:53 +0200

    Update major dependencies and fix eslint-loader bug

--- webpack.develop.js
+++ webpack.develop.js
@@ -2,6 +2,10 @@ const path = require('path');
 
 const libraryName = 'angularjs-nouislider';
 
+/* TODO: Remove this when new version of eslint-loader is released.
+ * See https://github.com/webpack-contrib/eslint-loader/issues/271 for more information */
+const esLintFormatter = require('eslint/lib/cli-engine/formatters/stylish');
+
 module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
@@ -17,6 +21,9 @@ module.exports = {
         exclude: /node_modules/,
         loader: 'eslint-loader',
         enforce: 'pre',
+        options: {
+          formatter: esLintFormatter,
+        },
       },
       {
         test: /\.js$/,
--- webpack.examples.js
+++ webpack.examples.js
@@ -1,9 +1,13 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
+
+/* TODO: Remove this when new version of eslint-loader is released.
+ * See https://github.com/webpack-contrib/eslint-loader/issues/271 for more information */
+const esLintFormatter = require('eslint/lib/cli-engine/formatters/stylish');
 
 module.exports = {
   entry: './src-examples/js/examples',
@@ -19,6 +23,9 @@ module.exports = {
         exclude: /node_modules/,
         loader: 'eslint-loader',
         enforce: 'pre',
+        options: {
+          formatter: esLintFormatter,
+        },
       },
       {
         test: /\.js$/,
